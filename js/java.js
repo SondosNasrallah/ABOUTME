b4403d1ee41d2b161fa617f92e7b183028769c25
Mahmoud-Barghouthi <mah.barghouthi47@gmail.com>
2021-04-27 16:40:39 +0300

    last edit

--- js/java.js
+++ js/java.js
@@ -8,7 +8,7 @@
 
 alert('Welcome to our humble page, where you can test your mind');
 let uName = prompt('Please Enter your name');
-alert('welcome to our page ' + uName + ' you can play the guessing game now')
+alert('welcome to our page ' + uName + ' you can play the guessing game now');
 
 alert('My Name start with MAHM and ends with OUD');
 
@@ -21,37 +21,42 @@ alert('You are GOD DAMN RIGHT');
 
 alert('Answer 5 Question to enter my HEART\n من الاخر بدي اقززك');
 
-
-let mGame = prompt('Am I a Gamer');
+let score = 0;
+let mGame = prompt('Am I a Gamer?');
 
 switch (mGame.toUpperCase()) {
 case 'YES':
 case 'Y':
   alert('You Are Rock');
+  score++;
   break;
 case 'NO':
 case 'N':
   alert('You lost a point');
+  score--;
   break;
 }
 
-let mWeight = prompt('Am I fat');
+let mWeight = prompt('Am I fat?');
 
 switch (mWeight.toUpperCase()) {
 case 'YES':
 case 'Y':
+  score - 2;
   alert('You lost 2 points');
   break;
 case 'NO':
 case 'N':
-  alert('Habibi, you are right');
+  score++;
+  alert('Habibi, you are right?');
   break;
 }
 
-let mTv = prompt('Am I a Breaking Bad Fans');
+let mTv = prompt('Am I a Breaking Bad Fans ?');
 switch (mTv.toUpperCase()) {
 case 'YES':
 case 'Y':
+  score++;
   alert('You Are SOOO RIGHT');
   break;
 case 'NO':
@@ -60,11 +65,12 @@ case 'N':
   break;
 }
 
-let mSport = prompt('Am I a Basketball fan');
+let mSport = prompt('Am I a Basketball fan?');
 switch (mSport.toUpperCase()) {
 case 'YES':
 case 'Y':
   alert('Yalaaaa, lets run');
+  score++;
   break;
 case 'NO':
 case 'N':
@@ -72,10 +78,11 @@ case 'N':
   break;
 }
 
-let mFood = prompt('Am I a food fan');
+let mFood = prompt('Am I a food fan?');
 switch (mFood.toUpperCase()) {
 case 'YES':
 case 'Y':
+  score++;
   alert('Burger is the besttt');
   break;
 case 'NO':
@@ -85,11 +92,10 @@ case 'N':
 }
 
 
-alert('Now game #2 \n I have a number and you need to guess it');
+alert('Now game #2 \nI have a number and you need to guess it');
 
 
 let userInput = parseInt(prompt('My number is between 10 to 20 and it is a single number'));
-let score = 0;
 let count = 0;
 
 for (let i = 0; i < 4; i++) {
@@ -117,7 +123,7 @@ let answer = prompt('what is my favorite food ?');
 answer = answer.toUpperCase();
 let counter = 0;
 for (let i = 0; i < 6; i++) {
-  if (answer === nFood[0] || answer === nFood[1]|| answer === nFood[2] || answer === nFood[3] || answer === nFood[4] || answer === nFood[5]) {
+  if (answer === nFood[0] || answer === nFood[1] || answer === nFood[2] || answer === nFood[3] || answer === nFood[4] || answer === nFood[5]) {
     alert('Correct ' + answer + ' is one of the favorite food');
     score++;
     break;
@@ -129,14 +135,14 @@ for (let i = 0; i < 6; i++) {
 }
 
 if (counter == 6) {
-  alert('you have consumed all your guesses and my favorite food are Munsef, Burger, Shawerma, Fries, Fish and Maqlobeh' );
+  alert('You have consumed all your guesses and my favorite food are the following \nMunsef, Burger, Shawerma, Fries, Fish and Maqlobeh');
 }
 
 console.log(score);
-alert( uName + ' Your score is ' + score)
+alert(uName + ' Your score is ' + score);
 
-if(score == 2){
+if (score > 5) {
   alert(uName + ' it seems that you are me');
-}else if(score < 2) {
+} else if (score <= 5) {
   alert(uName + ' , we msut to get to know each other more');
 }
